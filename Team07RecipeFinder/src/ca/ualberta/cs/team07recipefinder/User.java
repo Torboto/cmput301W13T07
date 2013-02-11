69ba1225aa1d7bead27aac88c8d09d309372d8d1
Adam St. Arnaud <ajstarna@ualberta.ca>
2013-02-10 21:31:54 -0700

    Is this working?

--- Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/User.java
+++ Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/User.java
@@ -1,5 +1,45 @@
 package ca.ualberta.cs.team07recipefinder;
 
-public class User {
 
+public class User {
+	private static User instance = null;
+	private String name;
+	private String email;
+	private Pantry pantry;
+	
+	protected User() {
+		
+	}
+	
+	public static User getInstance() {
+		if (instance == null) {
+			instance = new User();
+		}
+		return instance;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Pantry getPantry() {
+		return pantry;
+	}
+
+	public void setPantry(Pantry pantry) {
+		this.pantry = pantry;
+	}
+	
 }
