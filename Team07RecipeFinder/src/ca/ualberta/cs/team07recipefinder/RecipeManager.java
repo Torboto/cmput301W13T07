acec34efd5c17f9b2ed8f3148b5b39d9e298c26f
xiaohui <xiaohui6@ualberta.ca>
2013-02-15 11:19:02 -0700

    RecipeManager.java updated

--- Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/RecipeManager.java
+++ Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/RecipeManager.java
@@ -1,5 +1,34 @@
 package ca.ualberta.cs.team07recipefinder;
 
+import java.util.List;
+
 public class RecipeManager {
 
+	List<Recipe> recipes;
+	
+	public RecipeManager() {
+		
+	}
+	
+	// Search by a list of ingredients
+	public Recipe[] search(String[] ingredient) {
+		return null;
+	}
+	
+	// Search by recipe ID
+	public Recipe[] search(long recipeID) {
+		return null;
+	}
+	
+	// Search by recipe name
+	public Recipe[] search(String name) {
+		return null;
+	}
+	
+	/*
+	// Delete a recipe stored in server given recipe ID
+	public void delete(long recipeID) {
+		
+	}
+	*/
 }
