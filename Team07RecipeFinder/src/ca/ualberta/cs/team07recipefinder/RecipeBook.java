e4b0677cb85f8b5a482f17f10edae541464426bd
George Coomber <georgecoomber@gmail.com>
2013-03-03 09:53:19 -0700

    Making changes to recipebook class.

--- Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/RecipeBook.java
+++ Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/RecipeBook.java
@@ -1,7 +1,20 @@
 package ca.ualberta.cs.team07recipefinder;
 
+import java.util.List;
 
-public class RecipeBook extends Recipe {
+// GC - A model class that contains a list of the user's cached recipes, and
+//		methods for saving recipes, deleting recipes, and searching for recipes
+public class RecipeBook extends Model<View> {
+	// GC - a list that contains all of the user's locally cached recipes.
+	private List<Recipe> recipes;
 	
-
+	// GC - adds a recipe to the user's locally cached recipes
+	public void writeRecipe() {
+		
+	}
+	
+	// GC - deletes a recipe from the user's locally cached recipes
+	public void deleteRecipe() {
+			
+	}
 }
