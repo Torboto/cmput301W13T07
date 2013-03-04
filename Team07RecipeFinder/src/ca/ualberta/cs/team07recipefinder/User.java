b1aebdc6546e2ed84b3a6b6ab39588fad51e3e8d
Torboto <motifo@gmail.com>
2013-03-04 16:16:28 -0700

    Refactored UserActivity along with User model. Got rid of UserController as there is only one function (20lines) of code that accessed XML widgets.

--- Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/User.java
+++ Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/User.java
@@ -1,14 +1,22 @@
 package ca.ualberta.cs.team07recipefinder;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.io.StreamCorruptedException;
 
 import android.content.Context;
 
+/* 
+ * ET: Wrote 
+ */
 @SuppressWarnings("serial")
-public class User implements Serializable{
+public class User implements Serializable {
 	private static User instance = null;
 	private String name;
 	private String email;
@@ -17,7 +25,7 @@ public class User implements Serializable{
 
 	protected User() {
 	}
-	
+
 	public static User getInstance() {
 		if (instance == null) {
 			instance = new User();
@@ -25,7 +33,14 @@ public class User implements Serializable{
 		return instance;
 	}
 
-	public void Write(Context context) {
+	/*
+	 * ET: Sets name/email member variables for object, and then writes
+	 * serilized form to file userdata in the application data folder.
+	 */
+	public void Write(Context context, String inputEmail, String inputName) {
+		this.email = inputEmail;
+		this.name = inputName;
+
 		try {
 			context.deleteFile(filename);
 			FileOutputStream fout = context.openFileOutput(filename,
@@ -38,8 +53,66 @@ public class User implements Serializable{
 		}
 	}
 
-	public void emailRecipe(int recipeId) {
+	/*
+	 * ET: Reads in User object from userdata file in application data folder
+	 */
+	public void Read(Context context) {
+		User user = null;
+		FileInputStream fin = null;
+		ObjectInputStream ois = null;
 
+		try {
+			fin = context.openFileInput(this.getFilename());
+			ois = new ObjectInputStream(fin);
+			user = (User) ois.readObject();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (StreamCorruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		this.email = user.email;
+		this.name = user.name;
+		this.pantry = user.pantry;
+
+		try {
+			fin.close();
+			ois.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/*
+	 * ET: Checks to see if userdata exists in internal app data folder.
+	 */
+	public Boolean userExists(Context context) {
+		File file = context.getFileStreamPath(this.getFilename());
+
+		if (file.exists()) {
+			this.Read(context);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/*
+	 * Recipe will need a parser to compile it into a paintext format, this
+	 * method should not parse anything, simply send an email. May want to
+	 * change input to take in the paintext, and an email instead. Will require
+	 * looking into JavaEmailAPI.
+	 */
+	public void emailRecipe(int recipeId, String email) {
 	}
 
 	public String getName() {
--- Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/UserController.java
+++
@@ -1,6 +0,0 @@
-package ca.ualberta.cs.team07recipefinder;
-
-//GC: The controller for the User model class.
-public class UserController {
-
-}
