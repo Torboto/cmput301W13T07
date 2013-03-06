338c10d1964a74ed3d8bb35cdcf2eeb7cb243e0b
Adam St. Arnaud <ajstarna@ualberta.ca>
2013-03-05 17:17:56 -0700

    Working on NewRecipeActivity

---
+++ Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/NewRecipeActivity.java
@@ -0,0 +1,68 @@
+package ca.ualberta.cs.team07recipefinder;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.EditText;
+
+public class NewRecipeActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_new_recipe);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_new_recipe2, menu);
+		return true;
+	}
+	
+	public void back(View v) {
+		// AS: Go back to my recipes activity
+		finish();
+	}
+	
+	public void createRecipe(View v){
+		
+		EditText DescriptionEditText = getDescription();
+		EditText IngredientsEditText = getIngredients();
+		EditText DirectionsEditText = getDirections();
+		if ((!isEmpty(DescriptionEditText)) && (!isEmpty(IngredientsEditText)) &&
+				(!isEmpty(DirectionsEditText))){
+			/*
+			 *  AS: Now we know the required fields are filled in
+			 *   before we proceed
+			 */
+			RecipeBookController RBController = new RecipeBookController();
+		}
+	}
+	private boolean isEmpty(EditText etText) {
+		// AS: returns if an Edit Text is empty or not
+		if(etText.getText().toString().trim().length() > 0)
+		    return false;
+		 else
+		   return true; 
+	}
+	
+	private EditText getDescription(){
+		// AS: returns the description as an Edit Text
+		EditText DescriptionEditText = (EditText) findViewById(R.id.etDescription);
+		return DescriptionEditText;
+	}
+
+	private EditText getIngredients(){
+		// AS: returns the ingredients as an Edit Text
+		EditText IngredientsEditText = (EditText) findViewById(R.id.etNewIngredients);
+		return IngredientsEditText;
+	}
+	
+	private EditText getDirections(){
+		// AS: returns the new directions as an Edit Text
+		EditText IngredientsEditText = (EditText) findViewById(R.id.etNewIngredients);
+		return IngredientsEditText;
+	}
+	
+}
