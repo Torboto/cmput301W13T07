f75f450b71e10e209925175195905561d86a2206
Torboto <motifo@gmail.com>
2013-03-06 14:33:04 -0700

    RecipeController skeleton code

---
+++ Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/RecipeController.java
@@ -0,0 +1,24 @@
+package ca.ualberta.cs.team07recipefinder;
+
+public class RecipeController {
+
+	public void writeRecipe(Recipe recipe){
+	}
+
+    public void deleteRecipe(UUID uuid){
+    }
+
+    /*
+     * Write over old recipe with the same UUID
+     */
+    public void updateRecipe(Recipe recipe){
+    }
+	
+    public RecipeBook searchRecipeIngredients(ArrayList<String> ingredients){
+        return null;
+	}
+    
+    public RecipeBook searchRecipeTitles(String title){
+        return null;
+	}
+}
