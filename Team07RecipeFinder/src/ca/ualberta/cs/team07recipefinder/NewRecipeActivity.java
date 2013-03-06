5be9f0612691883ed725428349c21796fae59773
Adam St. Arnaud <ajstarna@ualberta.ca>
2013-03-05 18:10:33 -0700

    More done on NewRecipeActivity and RecipeBookController

--- Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/NewRecipeActivity.java
+++ Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/NewRecipeActivity.java
@@ -36,8 +36,15 @@ public class NewRecipeActivity extends Activity {
 			 *  AS: Now we know the required fields are filled in
 			 *   before we proceed
 			 */
+			String Description = DescriptionEditText.toString();
+			String Ingredients = IngredientsEditText.toString();
+			String Directions = DirectionsEditText.toString();
 			RecipeBookController RBController = new RecipeBookController();
+			//RBController.creatRecipe();
 		}
+		
+		// AS: If one or more fields empty could potentially have a 
+		// dialog saying so?
 	}
 	private boolean isEmpty(EditText etText) {
 		// AS: returns if an Edit Text is empty or not
@@ -46,6 +53,13 @@ public class NewRecipeActivity extends Activity {
 		 else
 		   return true; 
 	}
+	
+	//TODO: figure out title
+	//private EditText getTitle(){
+		// AS: returns the new directions as an Edit Text
+	//	EditText TitleEditText = (EditText) findViewById(R.id.etTitle);
+	//	return TitleEditText;
+	//}
 	
 	private EditText getDescription(){
 		// AS: returns the description as an Edit Text
@@ -64,5 +78,6 @@ public class NewRecipeActivity extends Activity {
 		EditText IngredientsEditText = (EditText) findViewById(R.id.etNewIngredients);
 		return IngredientsEditText;
 	}
+	
 	
 }
