dccfcce218280538498ef39e280d04ef9bcb8fc3
George Coomber <georgecoomber@gmail.com>
2013-03-07 09:32:40 -0700

    Changing sql client

--- Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/SqlClient.java
+++ Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/SqlClient.java
@@ -1,13 +1,11 @@
 package ca.ualberta.cs.team07recipefinder;
 
-import java.util.ArrayList;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import java.util.UUID;
 
 /*
@@ -98,37 +96,49 @@ public class SqlClient extends SQLiteOpenHelper {
     	// Return entry that has been read from database.
     	return recipe;
     }
-/*    
-    // Edit an existing entry
-    public void editEntry(CalorieEntry temp_entry, int id) {
+   /*
+    // GC: update an existing recipe in the database.
+    public void updateRecipe(Recipe temp_recipe, UUID recipeId) {
+    	
     	SQLiteDatabase db = this.getReadableDatabase();
 
     	// New value for one column
     	ContentValues values = new ContentValues();
     	
-    	values.put(COLUMN_NAME_ID, id);
-    	values.put(COLUMN_NAME_DESC, temp_entry.getDesc());
-    	values.put(COLUMN_NAME_TOTCAL, temp_entry.getTotalCal());
-    	values.put(COLUMN_NAME_CALPS, temp_entry.getCalPerServ());
-    	values.put(COLUMN_NAME_SERVS, temp_entry.getServSize());
-    	values.put(COLUMN_NAME_NUMS, temp_entry.getNumServ());
-    	values.put(COLUMN_NAME_HOUR, temp_entry.getETime().getHour());
-    	values.put(COLUMN_NAME_DAY, temp_entry.getETime().getDay());
-    	values.put(COLUMN_NAME_MONTH, temp_entry.getETime().getMonth());
-    	values.put(COLUMN_NAME_YEAR, temp_entry.getETime().getYear());
-    	values.put(COLUMN_NAME_CUSTOM, temp_entry.getisCustom());
+    	values.put(COLUMN_NAME_ID, String.valueOf(recipeId));
+    	values.put(COLUMN_NAME_CONTENT, temp_recipe.getDesc());
 
     	// Which row to update, based on the ID
     	String selection = COLUMN_NAME_ID + " LIKE ?";
-    	String[] selectionArgs = { String.valueOf(id) };
+    	String[] selectionArgs = { String.valueOf(recipeId) };
 
     	int count = db.update(
     	    TABLE_NAME,
     	    values,
     	    selection,
     	    selectionArgs);
-    }
+    }*/
     
+    // GC: Query the database to check if a Recipe exits with the recipeId
+    public boolean checkRow(UUID recipeId)
+    {
+		boolean isExist = false;
+		SQLiteDatabase db = this.getReadableDatabase();
+
+		// Define a projection that specifies which columns from the database
+		// we will use in the query
+		String[] projection = { COLUMN_NAME_ID };
+		// Query the database for the rows and columns we want.
+    	Cursor c = db.query(TABLE_NAME, projection, COLUMN_NAME_ID + "=?", 
+    			new String[] {String.valueOf(recipeId)},
+    			null, null, null, null);
+    			
+    	if(c != null) {
+    		isExist = true;
+    	}
+		return isExist;
+    }
+   /* 
     // Delete an existing entry
     public void deleteEntry(int id) {
     	SQLiteDatabase db = this.getReadableDatabase();
@@ -157,66 +167,6 @@ public class SqlClient extends SQLiteOpenHelper {
     	}
     	
     	return id_array;
-    }
-    
-    //Find the user's total calories over all entries.
-    public double findAllCals() {
-    	double tot_sum = 0;
-    	SQLiteDatabase db = this.getReadableDatabase();
-    	
-    	// Query the database for the rows and columns we want.
-    	Cursor c = db.rawQuery("SELECT * FROM " + TABLE_NAME, null);
-    			
-    	c.moveToFirst();
-    	
-    	
-    	// Add up all the calorie values.
-    	while (!c.isAfterLast()) {
-    	     tot_sum += Double.parseDouble(c.getString(2));
-    	     c.moveToNext();
-    	}
-    	
-    	// Return entry that has been read from database.
-    	return tot_sum;
-    }
-    
-    // Find the user's total comsumption time by adding the number
-    // distinct days in the database
-    public double findTotTime() {
-    	int num_days = 0;
-    	
-    	SQLiteDatabase db = this.getReadableDatabase();
-    	
-    	// Get all distrinct days from the database.
-    	Cursor c = db.rawQuery("SELECT DISTINCT " + COLUMN_NAME_DAY + " FROM " + TABLE_NAME, null);
-    			
-    	c.moveToFirst();
-    	
-    	// Add up all the calorie values.
-    	while (!c.isAfterLast()) {
-    	     num_days++;
-    	     c.moveToNext();
-    	}
-    	
-    	// Return entry that has been read from database.
-    	return (double) num_days;
-    }
-    
-    // Find the user's average calories consumed per day.
-    public double findAveCals() {
-    	double num_days;
-    	double tot_cals;
-    	
-    	tot_cals = this.findAllCals();
-    	num_days = this.findTotTime();
-    	
-    	// If the number of days is zero, set the number of days to 1
-    	// in order to avoid dividing by zero.
-    	if (num_days < 0.001)
-    		num_days = 1;
-    	
-    	// Return entry that has been read from database.
-    	return tot_cals/num_days;
     }
 */
 }
