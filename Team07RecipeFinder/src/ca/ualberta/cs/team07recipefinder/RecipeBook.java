760532574669dd7533b5da126d9ccfcf707c615e
Torboto <motifo@gmail.com>
2013-03-07 09:45:56 -0700

    Cleaning up

--- Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/RecipeBook.java
+++ Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/RecipeBook.java
@@ -1,13 +1,6 @@
 package ca.ualberta.cs.team07recipefinder;
 
 import java.util.ArrayList;
-import java.util.UUID;
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.client.methods.HttpPost;
-
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 
 public class RecipeBook extends Model<View> {
