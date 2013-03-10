e2b393ef6e3e16ee546368d595884d443b102a92
Adam St. Arnaud <ajstarna@ualberta.ca>
2013-03-09 17:01:22 -0700

    ViewRecipeActivty now will load from My Recipes, and the delete button is set up. TODO: load from search, display recipe information, and save button

--- Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/ViewRecipeActivity.java
+++ Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/ViewRecipeActivity.java
@@ -1,5 +1,7 @@
 package ca.ualberta.cs.team07recipefinder;
 
+import java.util.UUID;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,22 +15,30 @@ public class ViewRecipeActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_view_recipe);
 		
-		//Bundle extras = getIntent().getExtras();
-		//int code = extras.getInt("code");
-		int code = 0;
-		if (code == 0){
+		// AS: call fillRecipe() to get the information to be displayed about
+		// the current recipe
+		fillRecipe();
+		
+		Bundle extras = getIntent().getExtras();
+		int code = extras.getInt("code");
+		
+		// AS: depending on whether the user came from My Recipes or from a search
+		// we set up different buttons 
+		if (code == 1){
 			// AS: if came from My Recipes
 			fromMyRecipes();
-			Button deleteButton = (Button) findViewById(R.id.bDone);
+			Button deleteButton = (Button) findViewById(R.id.b_recipeDelete);
 			deleteButton.setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View v) {
 					// AS: The delete button calls deleteRecipe and finishes activity
-					deleteRecipe();
+					Bundle extras = getIntent().getExtras();
+					String recipeString = extras.getString("recipeID");
+					deleteRecipe(recipeString);
 					finish();
 				}
 			});
-		} else if (code == 1) {
+		} else if (code == 2) {
 			// AS: if came from Search
 			fromSearch();
 		} else{
@@ -36,13 +46,22 @@ public class ViewRecipeActivity extends Activity {
 		}
 	}
 
+	private void fillRecipe() {
+		// TOD
+		
+	}
+
 	private void fromMyRecipes(){
+		// AS: hides the save button since it is not available when we are viewing
+		// one of our own recipes
 		Button saveButton = (Button) findViewById(R.id.b_recipeSave);
 		saveButton.setVisibility(4);
 		return;
 	}
 	
 	private void fromSearch(){
+		// AS: hides the edit and delete buttons since they are not available
+		// when we are viewing a recipe from search
 		Button editButton = (Button) findViewById(R.id.b_recipeEdit);
 		Button deleteButton = (Button) findViewById(R.id.b_recipeDelete);
 		editButton.setVisibility(4);
@@ -50,8 +69,11 @@ public class ViewRecipeActivity extends Activity {
 		return;
 	}
 	
-	private void deleteRecipe(){
-		//AS: will need to delete the current recipe
+	private void deleteRecipe(String recipeString){
+		//AS: delete the current recipe specified by a string representation of a UUID
+		UUID recipeID = UUID.fromString(recipeString);
+		RecipeController rc = new RecipeController();
+		rc.deleteRecipe(recipeID);
 		return;
 	}
 	
