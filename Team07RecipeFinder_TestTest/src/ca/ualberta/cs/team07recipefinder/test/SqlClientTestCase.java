a712cdff123e37ec567dbeb1488f47e54c3fa594
George Coomber <georgecoomber@gmail.com>
2013-03-11 14:46:28 -0600

    Changing sql junit test.

--- Team07RecipeFinder_TestTest/src/ca/ualberta/cs/team07recipefinder/test/SqlClientTestCase.java
+++ Team07RecipeFinder_TestTest/src/ca/ualberta/cs/team07recipefinder/test/SqlClientTestCase.java
@@ -2,11 +2,15 @@ package ca.ualberta.cs.team07recipefinder.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import ca.ualberta.cs.team07recipefinder.Recipe;
 import ca.ualberta.cs.team07recipefinder.SqlClient;
 
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.test.ServiceTestCase;
 import junit.framework.TestCase;
@@ -15,13 +19,34 @@ import junit.framework.TestCase;
  * Recipes from the local SqlDatabase
  */
 public class SqlClientTestCase extends TestCase {
-	private SqlClient testClient = new SqlClient(getTestContext());
+	private SqlClient testClient;
+	Recipe testRecipe;
+	String recipeId;
 	
-	public void testConnection() {
+	@Before
+	public void setUp() throws Exception {
+		testClient = new SqlClient(getTestContext());
 		
+		ArrayList<String> ingredients = new ArrayList<String>();
+		ingredients.add("ingredient1");
+		ingredients.add("ingredient2");
+		
+		testRecipe = new Recipe("Title", "Description", 
+				ingredients, "Directions", "Email");
+		
+		recipeId = String.valueOf(testRecipe.getRecipeId());
+	}
+	
+	// Test if the Client has been instantiated.
+	public void testConnection() {
 		assertTrue(testClient != null);
 	}
 
+	
+	public void testWriteRecipe() {
+		
+		assertTrue(testClient != null);
+	}
 
 	private Context getTestContext() {
 		try {
