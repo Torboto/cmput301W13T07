50997e44ec2764ee2b65ef82bb7079a5d9fec182
George Coomber <georgecoomber@gmail.com>
2013-03-11 11:54:56 -0600

    SqlClient JUnit test.

---
+++ Team07RecipeFinder_TestTest/src/ca/ualberta/cs/team07recipefinder/test/TestSqlClient.java
@@ -0,0 +1,14 @@
+package ca.ualberta.cs.team07recipefinder.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestSqlClient {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
