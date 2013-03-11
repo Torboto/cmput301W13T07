d4b9759b0766247f409ef46776191eeb644abd23
Torboto <motifo@gmail.com>
2013-03-10 20:36:09 -0600

    Jdoc comments

--- Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/DataDownloadListener.java
+++ Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/DataDownloadListener.java
@@ -2,6 +2,10 @@ package ca.ualberta.cs.team07recipefinder;
 
 import java.util.ArrayList;
 
+/**
+ * @author Torboto
+ * Abstract type to be used as a trigger for async tasks to return data back to activity.
+ */
 public abstract interface DataDownloadListener {	
 	void dataDownloadedSuccessfully(ArrayList<Recipe> data);
 }
