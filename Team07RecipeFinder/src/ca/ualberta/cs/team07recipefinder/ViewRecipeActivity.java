ceb5b4ab4c32121f97f687a2d00ff9e3e4076b61
xiaohui <xiaohui@uwsclient-162-193.uws.ualberta.ca>
2013-03-27 17:29:26 -0600

    update ViewRecipeActivity.java

--- Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/ViewRecipeActivity.java
+++ Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/ViewRecipeActivity.java
@@ -14,7 +14,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
