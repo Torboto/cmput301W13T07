92acf64650dc47e61d844d6ca9a812df6d564a9e
George Coomber <georgecoomber@gmail.com>
2013-03-29 19:04:24 -0600

    Refactored the camera activity into a camera class.

---
+++ Team07RecipeFinder/src/ca/ualberta/cs/team07recipefinder/Camera.java
@@ -0,0 +1,41 @@
+package ca.ualberta.cs.team07recipefinder;
+
+import java.io.File;
+
+import android.os.Environment;
+
+/**
+ * A helper class that contains data and methods for the camera activity.
+ * @author gcoomber
+ *
+ */
+public class Camera {
+	private String folderName;
+	int imageNumber;
+	
+	public Camera(String folderName, int imageNumber) {
+		this.folderName = folderName;
+		this.imageNumber = imageNumber;
+	};
+	
+	/**
+	 * Gets the file with the file path that the image will be saved at.
+	 * @return
+	 */
+	public File getFile() {
+		String folder = Environment.getExternalStorageDirectory()
+				.getAbsolutePath() + "/tmp/" + this.folderName;
+		File folderF = new File(folder);
+		if (!folderF.exists()) {
+			folderF.mkdir();
+		}
+		
+		String imageFilePath = folder + "/" + folderName + "_"
+				+ String.valueOf(imageNumber) + ".jpg";
+		File imageFile = new File(imageFilePath);
+		
+		return imageFile;
+	}
+	
+
+}
