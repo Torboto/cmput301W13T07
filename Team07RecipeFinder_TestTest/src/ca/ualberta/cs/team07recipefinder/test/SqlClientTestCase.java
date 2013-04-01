e0804dcac9a822c569d40a917699007197a5c5e0
Torboto <motifo@gmail.com>
2013-04-01 15:22:32 -0600

    Updated sql junit stufF

--- Team07RecipeFinder_TestTest/src/ca/ualberta/cs/team07recipefinder/test/SqlClientTestCase.java
+++ Team07RecipeFinder_TestTest/src/ca/ualberta/cs/team07recipefinder/test/SqlClientTestCase.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.UUID;
 import android.test.AndroidTestCase;
 
-
 /* Test class for the SqlClient class. Tests adding, updating, and deleting
  * Recipes from the local SqlDatabase
  */
@@ -17,30 +16,27 @@ public class SqlClientTestCase extends AndroidTestCase {
 	Recipe testRecipe;
 	String recipeId;
 	UUID uId;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		testClient = new SqlClient(getContext());
 		ArrayList<String> ingredients = new ArrayList<String>();
 		ingredients.add("ingredient1");
 		ingredients.add("ingredient2");
-		
-		testRecipe = new Recipe("Title", "Description", 
-				ingredients, "Directions", "Email");
-		
+
+		testRecipe = new Recipe("Title", "Description", ingredients,
+				"Directions", "Email", Recipe.Location.LOCAL);
+
 		recipeId = String.valueOf(testRecipe.getRecipeId());
 		uId = testRecipe.getRecipeId();
 	}
+
 	/*
-	@Test
-	public void testConnection() {
-		testClient.deleteAllRows();
-		assertTrue(testClient.checkRow(uId) == false);
-		testClient.writeRecipe(testRecipe);
-		assertTrue(testClient.checkRow(uId) == true);
-		testClient.deleteAllRows();
-	}
-	*/
+	 * @Test public void testConnection() { testClient.deleteAllRows();
+	 * assertTrue(testClient.checkRow(uId) == false);
+	 * testClient.writeRecipe(testRecipe); assertTrue(testClient.checkRow(uId)
+	 * == true); testClient.deleteAllRows(); }
+	 */
 	@Test
 	public void testConnection() {
 		assertTrue(testClient != null);
@@ -56,74 +52,72 @@ public class SqlClientTestCase extends AndroidTestCase {
 		assertTrue(testClient.checkRow(uId) == false);
 		testClient.deleteAllRows();
 	}
-	
+
 	@Test
 	public void testReadRecipe() {
 		testClient.deleteAllRows();
 		Recipe tempRecipe;
-		
+
 		assertTrue(testClient.checkRow(uId) == false);
 		testClient.writeRecipe(testRecipe);
 		assertTrue(testClient.checkRow(uId) == true);
-		
+
 		tempRecipe = testClient.readRecipe(uId);
 
-		assertEquals(tempRecipe.getName(), testRecipe.getName());
+		assertEquals(tempRecipe.getTitle(), testRecipe.getTitle());
 		assertEquals(tempRecipe.getDescription(), testRecipe.getDescription());
-		assertEquals(tempRecipe.getIngredients().get(0),
-				testRecipe.getIngredients().get(0));
-		assertEquals(tempRecipe.getIngredients().get(1), 
-				testRecipe.getIngredients().get(1));
+		assertEquals(tempRecipe.getIngredients().get(0), testRecipe
+				.getIngredients().get(0));
+		assertEquals(tempRecipe.getIngredients().get(1), testRecipe
+				.getIngredients().get(1));
 		assertEquals(String.valueOf(tempRecipe.getRecipeId()),
 				String.valueOf(testRecipe.getRecipeId()));
-		assertEquals(tempRecipe.getDirections(), 
-				testRecipe.getDirections());
-		
+		assertEquals(tempRecipe.getDirections(), testRecipe.getDirections());
+
 		testClient.deleteRecipe(uId);
 		assertTrue(testClient.checkRow(uId) == false);
 		testClient.deleteAllRows();
 	}
-	
+
 	@Test
 	public void testUpdateRecipe() {
 		testClient.deleteAllRows();
 		assertTrue(testClient.checkRow(uId) == false);
 		testClient.writeRecipe(testRecipe);
 		assertTrue(testClient.checkRow(uId) == true);
-		
+
 		Recipe newRecipe = testRecipe;
 		newRecipe.setDescription("Description2");
 		newRecipe.setDirections("Directions2");
 		newRecipe.setName("Title2");
-		
+
 		testClient.updateRecipe(uId, newRecipe);
-		
+
 		assertTrue(testClient.checkRow(uId) == true);
-		
+
 		Recipe tempRecipe = testClient.readRecipe(uId);
-		
-		assertTrue(tempRecipe.getName().equals(newRecipe.getName()));
+
+		assertTrue(tempRecipe.getTitle().equals(newRecipe.getTitle()));
 		assertTrue(tempRecipe.getDescription().equals(
 				newRecipe.getDescription()));
-		assertTrue(tempRecipe.getIngredients().get(0).equals(
-				newRecipe.getIngredients().get(0)));
-		assertTrue(tempRecipe.getIngredients().get(1).equals(
-				newRecipe.getIngredients().get(1)));
+		assertTrue(tempRecipe.getIngredients().get(0)
+				.equals(newRecipe.getIngredients().get(0)));
+		assertTrue(tempRecipe.getIngredients().get(1)
+				.equals(newRecipe.getIngredients().get(1)));
 		assertTrue(String.valueOf(tempRecipe.getRecipeId()).equals(
 				String.valueOf(newRecipe.getRecipeId())));
-		assertTrue(tempRecipe.getDirections().equals(
-				newRecipe.getDirections()));
-		
+		assertTrue(tempRecipe.getDirections().equals(newRecipe.getDirections()));
+
 		testClient.deleteRecipe(uId);
-		assertTrue(testClient.checkRow(uId) == false);	
+		assertTrue(testClient.checkRow(uId) == false);
 		testClient.deleteAllRows();
 	}
-	
+
 	@Test
 	public void testGetAllRecipe() {
 		testClient.deleteAllRows();
 		ArrayList<Recipe> recipes = null;
-		
+
 		assertTrue(testClient.checkRow(uId) == false);
 		recipes = testClient.getAllRecipes();
 		assertTrue(recipes.size() == 0);
@@ -135,5 +129,5 @@ public class SqlClientTestCase extends AndroidTestCase {
 		assertTrue(testClient.checkRow(uId) == false);
 		testClient.deleteAllRows();
 	}
-	
+
 }
